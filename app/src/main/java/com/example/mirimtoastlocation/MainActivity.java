c792c4cb7daa62b2b232037ba0beb030d5908389
young <pieangel11@naver.com>
2020-11-16 22:25:33 +0900

    Incomplete Dialog

--- app/src/main/java/com/example/mirimtoastlocation/MainActivity.java
+++ app/src/main/java/com/example/mirimtoastlocation/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.mirimtoastlocation;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.Display;
 import android.view.Gravity;
@@ -18,24 +20,39 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Button btn = findViewById(R.id.btn);
+        Button btnDialog = findViewById(R.id.btn_dialog);
         btn.setOnClickListener(btnListener);
+        btnDialog.setOnClickListener(btnListener);
     }
 
     View.OnClickListener btnListener = new View.OnClickListener() {
         @Override
         public void onClick(View v) {
-            Toast toast = Toast.makeText(getApplicationContext(), "토스트 연습", Toast.LENGTH_SHORT);
-            Display display = ((WindowManager)getSystemService(WINDOW_SERVICE)).getDefaultDisplay();
-            int xOffset = (int)(Math.random() * display.getWidth());
-            int yOffset = (int)(Math.random() * display.getHeight());
-//            toast.setGravity(Gravity.TOP|Gravity.LEFT, xOffset, yOffset);
-            toast.setGravity(Gravity.TOP|Gravity.CENTER_HORIZONTAL, 0, 120);
-            toast.show();
+            switch (v.getId()){
+                case R.id.btn:
+                    Toast toast = Toast.makeText(getApplicationContext(), "토스트 연습", Toast.LENGTH_SHORT);
+                    Display display = ((WindowManager)getSystemService(WINDOW_SERVICE)).getDefaultDisplay();
+                    int xOffset = (int)(Math.random() * display.getWidth());
+                    int yOffset = (int)(Math.random() * display.getHeight());
+//                  toast.setGravity(Gravity.TOP|Gravity.LEFT, xOffset, yOffset);
+                    toast.setGravity(Gravity.TOP|Gravity.CENTER_HORIZONTAL, 0, 70);
+                    toast.show();
+                    break;
+                case R.id.btn_dialog:
+                    AlertDialog.Builder dialog = new AlertDialog.Builder(MainActivity.this);
+                    dialog.setTitle("알림사항");
+                    dialog.setMessage("코로나 확진자 진행상황입니다.");
+                    dialog.setIcon(R.mipmap.ic_flower2);
+                    dialog.setPositiveButton("확인", dialogListener);
+                    dialog.show();
+            }
         }
     };
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        return super.onCreateOptionsMenu(menu);
-    }
+    DialogInterface.OnClickListener dialogListener = new DialogInterface.OnClickListener() {
+        @Override
+        public void onClick(DialogInterface dialogInterface, int i) {
+            Toast.makeText(MainActivity.this, "대화상자의 확인 버튼을 클릭했네요.", Toast.LENGTH_SHORT).show();
+        }
+    };
 }
